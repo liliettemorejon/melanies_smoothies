6364ae816b257ede6f78d138f57b588d9a196f9d
Liliette Morejon Averhoff <lmore160@fiu.edu>
2026-02-13 13:36:20 -0500

    Update streamlit_app.py
    for jsona and App

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,16 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, '+ name_on_order +'!' , icon="✅")
 
+# New section to display smoothiefruit nutrition information
+import requests
+
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+
+st.text(smoothiefroot_response)
+
+
 
 
 
